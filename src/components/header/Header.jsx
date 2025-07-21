2f75723403b08b29ac5835cec052c564080a245c
vyom10445 <vyom.chaturvedi10445knp@gmail.com>
2025-07-21 18:47:27 +0530

    commit for changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,12 @@
 import "./header.css";
 
 export default function Header() {
-  return <div className="header">header</div>;
+  return (
+    <div className="header">
+      <div className="headerTitle">
+        <div className="headerTitleSM">React</div>
+        <div className="headerTitleLG">Blog</div>
+      </div>
+    </div>
+  );
 }
