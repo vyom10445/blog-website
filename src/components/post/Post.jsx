9da62cdae70e02bb168e9cea6b5bd9deef812d71
vyom10445 <vyom.chaturvedi10445knp@gmail.com>
2025-07-21 22:20:38 +0530

    add sidebar

---
+++ src/components/post/Post.jsx
@@ -0,0 +1,5 @@
+import "./post.css";
+
+export default function Post() {
+  return <div className="posts">Post</div>;
+}
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,36 @@
+import "./sidebar.css";
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebarItem">
+        <span className="sidebarTitle">ABOUT ME</span>
+        <img src="./car.jpg" alt="" />
+        <p>
+          I'm Vyom, currently figuring out React and built this blog site as
+          part of the process.
+        </p>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">CATEGORIES</span>
+        <ul className="sidebarList">
+          <li className="sidebarListItem">Life</li>
+          <li className="sidebarListItem">Music</li>
+          <li className="sidebarListItem">Sports</li>
+          <li className="sidebarListItem">Tech</li>
+          <li className="sidebarListItem">Style</li>
+          <li className="sidebarListItem">Cinema</li>
+        </ul>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">FOLLOW US</span>
+        <div className="sidebarSocial">
+          <i className="sidebarIcon fab fa-facebook-square"></i>
+          <i className="sidebarIcon fab fa-instagram-square"></i>
+          <i className="sidebarIcon fab fa-pinterest-square"></i>
+          <i className="sidebarIcon fab fa-twitter-square"></i>
+        </div>
+      </div>
+    </div>
+  );
+}
