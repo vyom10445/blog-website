dc046c799e119bebc7f459b085f0fce03346849f
vyom10445 <vyom.chaturvedi10445knp@gmail.com>
2025-07-21 18:53:30 +0530

    add header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,8 +4,8 @@ export default function Header() {
   return (
     <div className="header">
       <div className="headerTitle">
-        <div className="headerTitleSM">React</div>
-        <div className="headerTitleLG">Blog</div>
+        <div className="headerTitleSm">React</div>
+        <div className="headerTitleLg">Blog</div>
       </div>
     </div>
   );
