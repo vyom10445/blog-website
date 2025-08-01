dcd7e157fd2edc85005ccec980c2496371ede2e4
vyom10445 <vyom.chaturvedi10445knp@gmail.com>
2025-08-01 19:04:27 +0530

    make card dynamic on clicking(ing not loading)

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -2,11 +2,14 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import SinglePost from "../../components/singlePost/SinglePost";
 import "./single.css";
 import posts from "../../utils/Data";
+import { useParams } from "react-router-dom";
 
 export default function Single() {
+  const { postId } = useParams();
+  const postData = posts.find((post) => post.id.toString() === postId);
   return (
     <div className="single">
-      <SinglePost posts={postData} />
+      <SinglePost postData={postData} />
       <Sidebar />
     </div>
   );
